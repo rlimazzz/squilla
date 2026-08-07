757e246f0a993178cc82e325905a2a8e8399f472
Ryan <ryangabryel2005@gmail.com>
2026-08-07 16:31:21 -0300

    repl

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
+#include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
 
 typedef struct {
     char* buffer;
@@ -17,6 +19,28 @@ InputBuffer* new_input_buffer() {
     return input_buffer;
 }
 
+void print_prompt() {
+    printf("squilla > ");
+}
+
+void read_input(InputBuffer* input_buffer) {
+    ssize_t bytes_read = 
+        getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+
+    if (bytes_read <= 0) {
+        printf("Error reading input\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //Ignore trailing newline
+    input_buffer->input_length = bytes_read - 1;
+    input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+void close_input_buffer(InputBuffer* input_buffer) {
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
 
 int main(int argc, char *argv[]) {
     //REPL stands for Read Eval Print Loop
